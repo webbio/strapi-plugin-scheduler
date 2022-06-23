825a20376f9a5ecfb314196cd42ccb34bce05e2d
Rowan Paul <rowan-paul@webbio.nl>
2022-06-23 15:07:37 +0200

    feat: Move plugin to it's own card

--- admin/src/components/scheduler.js
+++ admin/src/components/scheduler.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { Box, Stack } from "@strapi/design-system";
-import { Divider } from "@strapi/design-system/Divider";
-import ScheduledPublish from "./publish";
-import DePublishScheduler from "./depublish";
+import React from 'react';
+import { Box, Stack } from '@strapi/design-system';
+import { Divider } from '@strapi/design-system/Divider';
+import { Typography } from '@strapi/design-system/Typography';
+import ScheduledPublish from './publish';
+import DePublishScheduler from './depublish';
 
 const scheduledPublish = () => {
   return (
-    <div>
+    <Box
+      as="aside"
+      aria-labelledby="scheduler"
+      background="neutral0"
+      borderColor="neutral150"
+      hasRadius
+      paddingBottom={4}
+      paddingLeft={4}
+      paddingRight={4}
+      paddingTop={6}
+      shadow="tableShadow"
+    >
+      <Typography variant="sigma" textColor="neutral600" id="scheduler">
+        Scheduler
+      </Typography>
       <Box padding={4}>
         <Divider />
       </Box>
@@ -18,7 +33,7 @@ const scheduledPublish = () => {
           <DePublishScheduler />
         </div>
       </Stack>
-    </div>
+    </Box>
   );
 };
 
