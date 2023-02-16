0568e4ca0f09c1f703f0914c610aaf8258aaa447
Cameron Paczek <cpazek@gmail.com>
2023-02-15 23:23:17 -0800

    convert to cron object format

--- cron-task.js
+++ cron-task.js
@@ -1,36 +1,39 @@
 module.exports = {
-  "*/30 * * * *": async ({ strapi }) => {
-    const posts = await strapi.db
-      .query("plugin::scheduler.scheduler")
-      .findMany({ where: { scheduledDatetime: { $lte: new Date() } } });
-          
-    posts.map(async (post) => {
-    if (post.scheduleType === "schedule") {
+  webbioSchedulerCron: {
+    rule: "*/30 * * * *",
+    task: async ({ strapi }) => {
+      const posts = await strapi.db
+        .query("plugin::scheduler.scheduler")
+        .findMany({ where: { scheduledDatetime: { $lte: new Date() } } });
 
-      await strapi.db.query(post.uid).update({
-        where: {
-          id: post.contentId,
-        },
-        data: {
-          publishedAt: new Date(),
-        },
+      posts.map(async (post) => {
+        if (post.scheduleType === "schedule") {
+
+          await strapi.db.query(post.uid).update({
+            where: {
+              id: post.contentId,
+            },
+            data: {
+              publishedAt: new Date(),
+            },
+          });
+          await strapi.db
+            .query("plugin::scheduler.scheduler")
+            .delete({ where: { id: post.id } });
+        } else if (post.scheduleType === "depublish") {
+          await strapi.db.query(post.uid).update({
+            where: {
+              id: post.contentId,
+            },
+            data: {
+              publishedAt: null,
+            },
+          });
+          await strapi.db
+            .query("plugin::scheduler.scheduler")
+            .delete({ where: { id: post.id } });
+        }
       });
-      await strapi.db
-        .query("plugin::scheduler.scheduler")
-        .delete({ where: { id: post.id } });
-    } else if (post.scheduleType === "depublish") {
-      await strapi.db.query(post.uid).update({
-        where: {
-          id: post.contentId,
-        },
-        data: {
-          publishedAt: null,
-        },
-      });
-      await strapi.db
-        .query("plugin::scheduler.scheduler")
-        .delete({ where: { id: post.id } });
-    }
-  });
-},
+    },
+  }
 };
