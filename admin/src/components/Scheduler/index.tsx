98f601039271e8fce9c70310c88d597d49f0dbc2
Martijn Webbio <martijn@webbio.nl>
2023-08-02 10:49:25 +0200

    feat: updated typescript waiting for strapi design system fix

--- admin/src/components/Scheduler/index.tsx
+++ admin/src/components/Scheduler/index.tsx
@@ -1,210 +1,190 @@
-import React, { useState, useEffect } from "react";
-import {
-  Box,
-  Stack,
-  Divider,
-  Typography,
-  DateTimePicker,
-} from "@strapi/design-system";
-import { useCMEditViewDataManager } from "@strapi/helper-plugin";
-import { useFetchClient } from "@strapi/helper-plugin";
+import React, { useState, useEffect, memo, useMemo } from 'react';
+import { Box, Stack, Divider, Typography } from '@strapi/design-system';
+import { useCMEditViewDataManager, useFetchClient } from '@strapi/helper-plugin';
+
+import { IScheduler, ISchedulerControllerReturn } from '../../../../server/controllers/scheduler';
+import { IConfig, IConfigControllerReturn } from '../../../../server/controllers/config';
+import DateTimePickerWrapper from '../date-time-picker-wrapper';
 
 const DATETIME_PICKER_STEP_SIZE = 5;
 
 const Scheduler = () => {
-  const form = useCMEditViewDataManager();
-  const axios = useFetchClient();
+	const { onChange, layout, initialData, modifiedData, isCreatingEntry } = useCMEditViewDataManager();
+	const { get } = useFetchClient();
 
-  const [isLoadingConfig, setIsLoadingConfig] = useState(true);
-  const [isLoadingScheduler, setIsLoadingScheduler] = useState(true);
+	const [isLoadingConfig, setIsLoadingConfig] = useState(true);
+	const [isLoadingScheduler, setIsLoadingScheduler] = useState(true);
 
-  const [config, setConfig] = useState({
-    config: undefined,
-  });
-  const [scheduler, setScheduler] = useState({
-    scheduler: undefined,
-  });
+	const [config, setConfig] = useState<IConfig | undefined | null>();
+	const [scheduler, setScheduler] = useState<IScheduler | undefined | null>(undefined);
 
-  const [publishAt, setPublishAt] = useState<any>(undefined);
-  const [archiveAt, setArchiveAt] = useState<any>(undefined);
+	const publishAt = useMemo(() => {
+		const newDate = modifiedData?.publishAt ?? initialData.publishAt;
+		return newDate ? new Date(newDate) : null;
+	}, [initialData.publishAt, modifiedData.publishAt]);
 
-  const updateFormValue = (name, value, initialValue = false) => {
-    if (value === null) {
-      form.onChange(
-        {
-          target: { name, value, type: "string" },
-        },
-        initialValue
-      );
-    } else {
-      form.onChange(
-        {
-          target: { name, value: value.toISOString(), type: "string" },
-        },
-        initialValue
-      );
-    }
-  };
+	const archiveAt = useMemo(() => {
+		const newDate = modifiedData?.archiveAt ?? initialData.archiveAt;
 
-  const handleChangePublishAt = (value) => {
-    setPublishAt(value);
-    updateFormValue("publishAt", value, false);
-  };
+		return newDate ? new Date(newDate) : null;
+	}, [initialData.archiveAt, modifiedData.archiveAt]);
 
-  const onClearPublishAt = () => {
-    setPublishAt(null);
-    updateFormValue("publishAt", null, false);
-  };
+	const updateFormValue = (name: string, value: Date | null, initialValue = false) => {
+		const stringDate = value ? value.toDateString() : null;
 
-  const handleChangeArchiveAt = (value) => {
-    setArchiveAt(value);
-    updateFormValue("archiveAt", value, false);
-  };
+		onChange(
+			{
+				target: { name, value: stringDate, type: 'string' }
+			},
+			initialValue
+		);
+	};
 
-  const onClearArchiveAt = () => {
-    setArchiveAt(null);
-    updateFormValue("archiveAt", null, false);
-  };
+	const handleChangePublishAt = (value: Date | null) => {
+		updateFormValue('publishAt', value, false);
+	};
 
-  const fetchConfig = async (uid) => {
-    try {
-      const configFetchResult = await axios.get(`/scheduler/config/${uid}`);
-      const { data: config } = configFetchResult?.data;
-      setConfig(config);
-    } catch (error) {
-    } finally {
-      setIsLoadingConfig(false);
-    }
-  };
+	const onClearPublishAt = () => {
+		updateFormValue('publishAt', null, false);
+	};
 
-  const getInitialPublishAt = (scheduler, config) => {
-    if (scheduler?.publishAt === null || scheduler?.publishAt !== undefined) {
-      return scheduler.publishAt;
-    }
+	const handleChangeArchiveAt = (value: Date | null) => {
+		updateFormValue('archiveAt', value, false);
+	};
 
-    if (
-      config?.initialPublishAtDate === null ||
-      config?.initialPublishAtDate !== undefined
-    ) {
-      return config.initialPublishAtDate;
-    }
+	const onClearArchiveAt = () => {
+		updateFormValue('archiveAt', null, false);
+	};
 
-    return undefined;
-  };
+	const fetchConfig = async (uid: string) => {
+		try {
+			const configFetchResult: { data?: IConfigControllerReturn } = await get(`/scheduler/config/${uid}`);
+			const { data: config } = configFetchResult?.data || {};
+			setConfig(config);
+		} catch (error) {
+		} finally {
+			setIsLoadingConfig(false);
+		}
+	};
 
-  const getInitialArchiveAt = (scheduler, config) => {
-    if (scheduler?.archiveAt === null || scheduler?.archiveAt !== undefined) {
-      return scheduler.archiveAt;
-    }
+	const fetchScheduler = async (uid: string, entryId: string) => {
+		try {
+			const schedulerFetchResult: { data: ISchedulerControllerReturn } = await get(
+				`/scheduler/scheduler/${uid}/${entryId}`
+			);
+			const { data: scheduler } = schedulerFetchResult?.data;
+			setScheduler(scheduler);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsLoadingScheduler(false);
+		}
+	};
 
-    if (
-      config?.initialArchiveAtDate === null ||
-      config?.initialArchiveAtDate !== undefined
-    ) {
-      return config.initialArchiveAtDate;
-    }
+	useEffect(() => {
+		const uid = layout.uid;
+		const entryId = initialData?.id;
+		fetchConfig(uid);
 
-    return undefined;
-  };
+		if (entryId) {
+			fetchScheduler(uid, entryId);
+		}
 
-  const fetchScheduler = async (uid, entryId) => {
-    try {
-      const schedulerFetchResult = await axios.get(
-        `/scheduler/scheduler/${uid}/${entryId}`
-      );
-      const { data: scheduler } = schedulerFetchResult?.data;
-      setScheduler(scheduler);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoadingScheduler(false);
-    }
-  };
+		if (isCreatingEntry) {
+			setIsLoadingScheduler(false);
+		}
+	}, []);
 
-  useEffect(() => {
-    const uid = form.layout.uid;
-    const entryId = form?.initialData?.id;
-    fetchConfig(uid);
+	useEffect(() => {
+		if (isLoadingConfig === false && isLoadingScheduler === false) {
+			const initialPublishAt = getInitialPublishAt(scheduler, config);
+			const initialArchiveAt = getInitialArchiveAt(scheduler, config);
 
-    if (entryId) {
-      fetchScheduler(uid, entryId);
-    }
+			if (initialPublishAt !== undefined) {
+				const value = initialPublishAt === null ? null : new Date(initialPublishAt);
+				updateFormValue('publishAt', value, true);
+			}
 
-    if (form.isCreatingEntry) {
-      setIsLoadingScheduler(false);
-    }
-  }, []);
+			if (initialArchiveAt !== undefined) {
+				const value = initialArchiveAt === null ? null : new Date(initialArchiveAt);
+				updateFormValue('archiveAt', value, true);
+			}
+		}
+	}, [isLoadingConfig, isLoadingScheduler]);
 
-  useEffect(() => {
-    if (isLoadingConfig === false && isLoadingScheduler === false) {
-      const initialPublishAt = getInitialPublishAt(scheduler, config);
-      const initialArchiveAt = getInitialArchiveAt(scheduler, config);
+	if (!config) {
+		return null;
+	}
 
-      if (initialPublishAt !== undefined) {
-        const value =
-          initialPublishAt === null ? null : new Date(initialPublishAt);
-        setPublishAt(value);
-        updateFormValue("publishAt", value, true);
-      }
-
-      if (initialArchiveAt !== undefined) {
-        const value =
-          initialArchiveAt === null ? null : new Date(initialArchiveAt);
-        setArchiveAt(value);
-        updateFormValue("archiveAt", value, true);
-      }
-    }
-  }, [isLoadingConfig, isLoadingScheduler]);
-
-  return config ? (
-    <Box
-      as="aside"
-      aria-labelledby="scheduler"
-      background="neutral0"
-      borderColor="neutral150"
-      hasRadius
-      paddingBottom={4}
-      paddingLeft={4}
-      paddingRight={4}
-      paddingTop={6}
-      shadow="tableShadow"
-    >
-      <Typography variant="sigma" textColor="neutral600" id="scheduler">
-        Scheduler
-      </Typography>
-      <Box marginTop={2} marginBottom={4}>
-        <Divider />
-      </Box>
-      <Stack spacing={2}>
-        <DateTimePicker
-          ariaLabel="Publish at"
-          clearLabel="Clear"
-          disabled={false}
-          label="Publish at"
-          name="publishAt"
-          onChange={handleChangePublishAt}
-          onClear={onClearPublishAt}
-          size="S"
-          step={DATETIME_PICKER_STEP_SIZE}
-          value={publishAt}
-          error={null}
-        />
-        <DateTimePicker
-          ariaLabel="Archive at"
-          clearLabel="Clear"
-          disabled={false}
-          label="Archive at"
-          name="archiveAt"
-          onChange={handleChangeArchiveAt}
-          onClear={onClearArchiveAt}
-          size="S"
-          step={DATETIME_PICKER_STEP_SIZE}
-          value={archiveAt}
-          error={null}
-        />
-      </Stack>
-    </Box>
-  ) : null;
+	return (
+		<Box
+			as="aside"
+			aria-labelledby="scheduler"
+			background="neutral0"
+			borderColor="neutral150"
+			hasRadius
+			paddingBottom={4}
+			paddingLeft={4}
+			paddingRight={4}
+			paddingTop={6}
+			shadow="tableShadow"
+		>
+			<Typography variant="sigma" textColor="neutral600" id="scheduler">
+				Scheduler
+			</Typography>
+			<Box marginTop={2} marginBottom={4}>
+				<Divider />
+			</Box>
+			<Stack spacing={2}>
+				<DateTimePickerWrapper
+					ariaLabel="Publish at"
+					label="Publish at"
+					name="publishAt"
+					onChange={handleChangePublishAt}
+					onClear={onClearPublishAt}
+					size="S"
+					step={DATETIME_PICKER_STEP_SIZE}
+					value={publishAt}
+					error={null}
+				/>
+				<DateTimePickerWrapper
+					ariaLabel="Archive at"
+					label="Archive at"
+					name="archiveAt"
+					onChange={handleChangeArchiveAt}
+					onClear={onClearArchiveAt}
+					size="S"
+					step={DATETIME_PICKER_STEP_SIZE}
+					value={archiveAt}
+					error={null}
+				/>
+			</Stack>
+		</Box>
+	);
 };
 
 export default Scheduler;
+
+const getInitialPublishAt = (scheduler?: IScheduler | null, config?: IConfig | null) => {
+	if (scheduler?.publishAt === null || scheduler?.publishAt !== undefined) {
+		return scheduler.publishAt;
+	}
+
+	if (config?.initialPublishAtDate === null || config?.initialPublishAtDate !== undefined) {
+		return config.initialPublishAtDate;
+	}
+
+	return undefined;
+};
+
+const getInitialArchiveAt = (scheduler?: IScheduler | null, config?: IConfig | null) => {
+	if (scheduler?.archiveAt === null || scheduler?.archiveAt !== undefined) {
+		return scheduler.archiveAt;
+	}
+
+	if (config?.initialArchiveAtDate === null || config?.initialArchiveAtDate !== undefined) {
+		return config.initialArchiveAtDate;
+	}
+
+	return undefined;
+};
--- server/controllers/scheduler.ts
+++ server/controllers/scheduler.ts
@@ -1,34 +1,42 @@
-import { Strapi } from "@strapi/strapi";
+import { Strapi } from '@strapi/strapi';
+
+export interface IScheduler {
+	uid: string;
+	entryId: number;
+	publishAt: string;
+	archiveAt: string;
+}
+
+export interface ISchedulerControllerReturn {
+	data: IScheduler | null;
+}
 
 export default ({ strapi }: { strapi: Strapi }) => ({
-  async getByUidAndEntryId(ctx) {
-    const { uid, entryId } = ctx.params;
+	async getByUidAndEntryId(ctx: any): Promise<ISchedulerControllerReturn> {
+		const { uid, entryId } = ctx.params;
 
-    if (!uid) {
-      throw new Error();
-    }
+		if (!uid) {
+			throw new Error();
+		}
 
-    if (!entryId) {
-      throw new Error();
-    }
+		if (!entryId) {
+			throw new Error();
+		}
 
-    const result = await strapi
-      .plugin("scheduler")
-      .service("scheduler")
-      .getByUidAndEntryId(uid, entryId);
+		const result = await strapi.plugin('scheduler').service('scheduler').getByUidAndEntryId(uid, entryId);
 
-    const existingPublish = result.find((entry) => entry.type === "publish");
-    const existingArchive = result.find((entry) => entry.type === "archive");
+		const existingPublish = result.find((entry: any) => entry.type === 'publish');
+		const existingArchive = result.find((entry: any) => entry.type === 'archive');
 
-    const data = {
-      uid,
-      entryId,
-      publishAt: existingPublish?.datetime,
-      archiveAt: existingArchive?.datetime,
-    };
+		const data = {
+			uid,
+			entryId,
+			publishAt: existingPublish?.datetime,
+			archiveAt: existingArchive?.datetime
+		};
 
-    return {
-      data,
-    };
-  },
+		return {
+			data
+		};
+	}
 });
